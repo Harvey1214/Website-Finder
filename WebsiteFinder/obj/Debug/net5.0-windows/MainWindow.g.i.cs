b967e76583ba8539ed10c6cfc961bc46ec0ddb45
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-06-06 15:42:55 +0200

    Update

--- WebsiteFinder/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ WebsiteFinder/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -62,7 +62,7 @@ namespace WebsiteFinder {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WebsiteFinder;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WebsiteFinder;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
