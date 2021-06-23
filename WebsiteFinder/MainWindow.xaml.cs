9a3386b1055294ba0627057c9c15f445a2dc5866
Mikuláš Hoblík <mikuhoblik@gmail.com>
2021-06-23 16:12:22 +0200

    Update

--- WebsiteFinder/MainWindow.xaml.cs
+++ WebsiteFinder/MainWindow.xaml.cs
@@ -182,3 +182,52 @@ namespace WebsiteFinder
         }
     }
 }
+
+
+/*
+ * <Grid ShowGridLines="True">
+        <Grid.ColumnDefinitions>
+            <ColumnDefinition Width="auto" MaxWidth="350px" />
+            <ColumnDefinition Width="auto" MaxWidth="100px"/>
+            <ColumnDefinition Width="auto" />
+            <ColumnDefinition Width="auto" />
+            <ColumnDefinition Width="auto" />
+            <ColumnDefinition Width="auto" />
+        </Grid.ColumnDefinitions>
+        <Grid.RowDefinitions>
+            <RowDefinition Height="auto" />
+            <RowDefinition Height="auto" />
+            <RowDefinition Height="auto" />
+            <RowDefinition Height="auto" />
+            <RowDefinition Height="auto" />
+            <RowDefinition Height="auto" />
+        </Grid.RowDefinitions>
+
+        <TextBox Grid.Row="0" Grid.Column="0" mah:TextBoxHelper.Watermark="Keywords (space separated)" x:Name="keywordsTextBox" HorizontalAlignment="Left" Margin="10,10,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="289" Height="26"/>
+        <mah:NumericUpDown Grid.Row="0" Grid.Column="1" TextAlignment="Left" mah:TextBoxHelper.Watermark="From Page" x:Name="minPagesNumericUpDown" HorizontalAlignment="Left" Margin="304,10,0,0" VerticalAlignment="Top" Width="118" Height="26"/>
+        <mah:NumericUpDown Grid.Row="0" Grid.Column="2" TextAlignment="Left" mah:TextBoxHelper.Watermark="Pages" x:Name="pagesNumericUpDown" HorizontalAlignment="Left" Margin="427,10,0,0" VerticalAlignment="Top" Width="90" Height="26"/>
+
+        <Button Grid.Row="1" Grid.Column="0" x:Name="startButton" Style="{StaticResource MahApps.Styles.Button.Flat}" Content="Search" HorizontalAlignment="Left" Margin="10,41,0,0" VerticalAlignment="Top" Click="startButton_Click" Width="158" Height="29"/>
+        <Button Grid.Row="1" Grid.Column="1" x:Name="startFromMailingList" Style="{StaticResource MahApps.Styles.Button.Flat}" Content="Mailing List" HorizontalAlignment="Left" Margin="173,41,0,0" VerticalAlignment="Top" Width="168" Click="startFromMailingList_Click" Height="29"/>
+        <TextBox Grid.Row="1" Grid.Column="2" mah:TextBoxHelper.Watermark="Min Age" x:Name="minDateTextBox" HorizontalAlignment="Left" Margin="346,44,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="82" Height="26"/>
+        <TextBox Grid.Row="1" Grid.Column="3" mah:TextBoxHelper.Watermark="Max Age" x:Name="maxDateTextBox" HorizontalAlignment="Left" Margin="434,44,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="83" Height="26"/>
+
+        <mah:MetroProgressBar Grid.Row="2" Grid.Column="0" x:Name="progressBar" HorizontalAlignment="Center" Height="27" Margin="0,75,0,0" VerticalAlignment="Top" Width="507"/>
+        <mah:ProgressRing Grid.Row="2" Grid.Column="0" x:Name="progressRing" IsActive="False" HorizontalAlignment="Left" Margin="485,72,0,0" VerticalAlignment="Top" Height="32" Width="32"/>
+        
+        <DataGrid Grid.Row="3" Grid.Column="0" IsReadOnly="True" x:Name="websitesDataGrid" Margin="10,107,10,182"/>
+
+        <CheckBox Grid.Row="4" Grid.Column="0" x:Name="showWindowCheckBox" Content="Show Browser Window" HorizontalAlignment="Left" Margin="10,361,0,0" VerticalAlignment="Top" Height="18" Width="145"/>
+        <CheckBox Grid.Row="4" Grid.Column="0" x:Name="filterByFooterDateCheckBox" Content="Filter by Footer Date (may eradicate a lot of results)" HorizontalAlignment="Left" Margin="11,384,0,0" VerticalAlignment="Top" Width="313" Height="18"/>
+        <Button Grid.Row="4" Grid.Column="1" Style="{StaticResource MahApps.Styles.Button.Flat}" x:Name="exportToCSV" Content="Export to CSV" HorizontalAlignment="Left" Margin="414,356,0,0" VerticalAlignment="Top" Click="exportToCSV_Click" Grid.ColumnSpan="3" Height="29" Width="105"/>
+
+        <TextBox Grid.Row="5" Grid.Column="0" mah:TextBoxHelper.Watermark="Gmail Address" x:Name="emailTextBox" HorizontalAlignment="Left" Margin="4,407,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="254" Height="26"/>
+        <PasswordBox Grid.Row="5" Grid.Column="1" mah:TextBoxHelper.Watermark="Password" x:Name="passwordBox" HorizontalAlignment="Left" Margin="264,407,0,0" VerticalAlignment="Top" Width="246" Height="26"/>
+
+        <TextBox Grid.Row="6" Grid.Column="0" mah:TextBoxHelper.Watermark="Subject" x:Name="subjectTextBox" HorizontalAlignment="Left" Margin="4,438,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="126" Height="26"/>
+        <TextBox Grid.Row="6" Grid.Column="1" mah:TextBoxHelper.Watermark="Body" x:Name="bodyTextBox" HorizontalAlignment="Left" Margin="138,438,0,0" TextWrapping="Wrap" VerticalAlignment="Top" Width="372" Height="65"/>
+        <Button Grid.Row="6" Grid.Column="0" x:Name="sendEmailsButton" Style="{StaticResource MahApps.Styles.Button.Flat}" Content="Email All" HorizontalAlignment="Left" Margin="4,469,0,0" VerticalAlignment="Top" Click="sendEmailsButton_Click" Width="126" Height="34"/>
+        
+        
+    </Grid>
+*/
